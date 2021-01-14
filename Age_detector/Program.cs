793edadd808e262a2de90f93d040fc8b1575da71
muhammadSalar589 <muhammad.427252@spu.edu.iq>
2021-01-14 15:38:46 +0300

    first commit

---
+++ Age_detector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Age_detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            int age;
+            int year;
+
+            for(int i=0; i < 10; i++)
+            {
+                Console.Write("Enter Your Name: ");
+                name = Console.In.ReadLine();
+                Console.WriteLine("-------------------------------");
+
+                Console.Write("Enter Your age: ");
+                age = int.Parse(Console.ReadLine());
+                Console.WriteLine("-------------------------------");
+
+                DateTime dateDate = DateTime.Today;
+ 
+                int result = dateDate.Year;
+                Console.WriteLine($"Now is {dateDate} So Your Birth date is:  {mwalid(age,result)} ");
+
+            }
+        }
+
+        static int mwalid(int age, int year)
+        {
+            int mwalid;
+
+            mwalid = year - age;
+            return mwalid;
+        }
+    }
+}
